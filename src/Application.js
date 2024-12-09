a1ee9da71d8c1ee7b4b02a373cd5996315d7ed6d
Lyndon King Obenza <zobenza115@gmail.com>
2024-12-10 03:55:23 +0800

    initial trees for now

--- src/Application.js
+++ src/Application.js
@@ -9,6 +9,8 @@ export class Application {
         this.libraryBuilding = new ModelLoader(this.sceneSetup.scene);
         this.csuOval = new ModelLoader(this.sceneSetup.scene);
         this.hinangBuilding = new ModelLoader(this.sceneSetup.scene);
+        this.treesModel = new ModelLoader(this.sceneSetup.scene);
+        
         this.init();
     }
     
@@ -18,7 +20,7 @@ export class Application {
         this.libraryBuilding.loadModel('/assets/3d_models/library_building.glb',{ x: 119, y: 0.35, z: 120 },{ x: 0.4, y: 0.4, z: 0.4 },{ x: 0, y: Math.PI/1.5, z: 0 });
         this.csuOval.loadModel('/assets/3d_models/csu_oval.glb',{ x: 30, y: 1, z: -280},{ x: 0.6, y: 0.6, z: 0.6 },{ x: 0, y:Math.PI, z: 0 });
         this.hinangBuilding.loadModel('/assets/3d_models/hinang_building.glb',{ x: -40, y: 0, z: -450},{ x: 1.1, y: 1.1, z: 1.1 });
-
+        this.treesModel.loadModel('/assets/3d_models/lowPolyTree.glb',{ x: -150, y: 0, z: 300},{ x: 9, y: 9, z: 9});
         this.animate();
     }
 
