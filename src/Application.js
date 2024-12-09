353386cfcb1f3b59ac25c858bb0295d519d3b0a6
Lyndon King Obenza <zobenza115@gmail.com>
2024-12-09 23:37:02 +0800

    forgot what I added here

--- src/Application.js
+++ src/Application.js
@@ -15,7 +15,7 @@ export class Application {
     init() {
         this.heroBuilding.loadModel('/assets/3d_models/new_admin_building.glb', { x: -120, y: -0.1, z: -60 },{ x: 1.4, y: 1.4, z: 1.4 });
         this.kinaadmanBuilding.loadModel('/assets/3d_models/kinaadman_building.glb',{ x: -120, y: -0.34, z: -240},{ x: 1.2, y: 1.2, z: 1.2 });
-        this.libraryBuilding.loadModel('/assets/3d_models/library_building.glb',{ x: 119, y: 0.5, z: 120 },{ x: 0.4, y: 0.4, z: 0.4 },{ x: 0, y: Math.PI/1.5, z: 0 });
+        this.libraryBuilding.loadModel('/assets/3d_models/library_building.glb',{ x: 119, y: 0.35, z: 120 },{ x: 0.4, y: 0.4, z: 0.4 },{ x: 0, y: Math.PI/1.5, z: 0 });
         this.csuOval.loadModel('/assets/3d_models/csu_oval.glb',{ x: 30, y: 1, z: -280},{ x: 0.6, y: 0.6, z: 0.6 },{ x: 0, y:Math.PI, z: 0 });
         this.hinangBuilding.loadModel('/assets/3d_models/hinang_building.glb',{ x: -40, y: 0, z: -450},{ x: 1.1, y: 1.1, z: 1.1 });
 
