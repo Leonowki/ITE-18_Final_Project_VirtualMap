29b0960e8d83a470ef215768febf800334615498
Lyndon King Obenza <zobenza115@gmail.com>
2024-12-26 19:44:46 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
   base: './', // Use relative paths for production
   build: {
     outDir: 'dist', // Output directory
-    chunkSizeWarningLimit: 1000, // Optional: Increase the chunk size warning limit
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'), // Main entry point
-        virtualmap: path.resolve(__dirname, 'virtualmap.html'), // Secondary page
+        main: './index.html', // Main entry point
+        virtualmap: './virtualmap.html', // Secondary page
       },
     },
   },
-  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.json', '**/*.webp'], // Include static file types
-  publicDir: 'public', // Specify a public directory for static files
 });
