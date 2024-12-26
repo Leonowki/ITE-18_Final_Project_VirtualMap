f1aabfb2d8fc4524fb335bcba0c70ca715ae8106
Lyndon King Obenza <zobenza115@gmail.com>
2024-12-26 16:41:02 +0800

    edit file directory to enable final deployement

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // Use relative paths for production
+  build: {
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: {
+        main: './index.html', // Main entry point
+        virtualmap: './virtualmap.html', // Secondary page
+      },
+    },
+  },
+});
